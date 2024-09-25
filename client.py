579c2dcfcb9f306b644afc4cd66efd492c67367e
arpan-69 <arpanofficial321@gmail.com>
2024-09-25 23:57:40 +0530

    Update client.py

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ client.connect(("localhost",9999))
 while True:
     client.send(input("Client: ").encode('utf-8'))
     message = client.recv(1024).decode('utf-8')
+    print("Server:", message)
     if message == 'quit':
         break
-    print("Server:",message)
 client.close()

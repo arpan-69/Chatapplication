c16781fcd795859bf65650907e0528593ceb5769
arpan-69 <arpanofficial321@gmail.com>
2024-09-25 23:56:53 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,22 @@
 import socket
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind(("localhost",9999))
+print("Server is listening...")
 server.listen()
 client, addr = server.accept()
+
 while True:
     message = client.recv(1024).decode('utf-8')
     if message == "quit":
+        print("Client has requested to end the chat.")
         client.send("quit".encode('utf-8'))
-        print("Client:", message)
-        break
+        break  # Exit the loop and stop receiving messages
     print("Client:", message)
-    client.send(input("Server: ").encode('utf-8'))
-
-server.close()
+    response = input("Server: ")
+    if response == "quit":
+        client.send("quit".encode('utf-8'))
+        break  # Server decides to end the chat
+    client.send(response.encode('utf-8'))
 client.close()
+server.close()
+print('Server closed.')
